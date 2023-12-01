85818348cb273e6841a725b275171ce557d8c5d5
rjswh0503 <rjswh0503@yahoo.com>
2023-12-01 12:51:01 +0900

    spring

---
+++ spring-JPA-MyShop/src/main/java/com/kh/spring/shop/SpringJapMyShopApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJapMyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJapMyShopApplication.class, args);
+	}
+
+}
---
+++ spring-JPA-MyShop/src/test/java/com/kh/spring/shop/SpringJapMyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJapMyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
